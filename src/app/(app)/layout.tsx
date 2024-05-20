a88bbc0d7a55ad52a8b1c0b69e13abc35d78ae9a
Lorenzo (adsion) <Lorenzo.magni@adsion.fr>
2024-05-20 10:11:51 +0200

    correct redirect

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,6 +1,18 @@
+import { auth } from "@/auth";
 import { DashboardNavigation } from "@/components/navigation/DashboardNavigation";
 import type { LayoutParams } from "@/types/next";
+import { redirect } from "next/navigation";
 
 export default async function RouteLayout(props: LayoutParams<{}>) {
+  const session = await auth();
+
+  if (!session) {
+    redirect("/login");
+  }
+
+  if (!session.user) {
+    redirect("/login");
+  }
+
   return <DashboardNavigation>{props.children}</DashboardNavigation>;
 }
--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -1,18 +1,6 @@
-import { auth } from "@/auth";
 import { AppPage } from "@/components/pages/AppPage";
-import { redirect } from "next/navigation";
 
 async function HomePage() {
-  const session = await auth();
-
-  if (!session) {
-    redirect("/login");
-  }
-
-  if (!session.user) {
-    redirect("/login");
-  }
-
   return <AppPage />;
 }
 
